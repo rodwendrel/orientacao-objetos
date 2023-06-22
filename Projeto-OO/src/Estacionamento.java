42c2aa11ed8ec420fb208fb3ad0412654b9c781f
BiancaPatrocinio7 <biancapatrocinio774@gmail.com>
2023-06-21 23:58:46 -0300

    Alerei a Main e a classe Estacionamento

--- Projeto-OO/src/Estacionamento.java
+++ Projeto-OO/src/Estacionamento.java
@@ -1,33 +1,119 @@
 public class Estacionamento {
 
-    // Atributos
-    private double valorPorcentagemContratante, valorDescontoHc, valorEstacionamento;
-    private int capacidadeEstacionamento, acessoFracao;
-    //private Tempo horarioAbertura, horarioEncerramento;
-    //private TipoAcesso valoresAcessos;
-    //private Acesso acessoEspecificoEstacionamento;
+    private int capacidade;
+    private float  valorFracao;
+    private int pocentagemHoraCheia;
+    private float valorDiaria;
+    private int porcentagemNoturno;
 
+    //private float horaCheia;
 
-    //Métodos
-    public boolean validarValoresAcessos(){
-        return validarValoresAcessos();
-    };
+    private float valorMesalista;
 
-    //setando
-    public void setAcessoFracao(int acessofracao){
-        this.acessoFracao = acessofracao;
+    private float valorEvento;
+
+    public int getCapacidade() {
+        return capacidade;
     }
-    public int getAcessoFracao(){
-        return acessoFracao;
+    public void setCapacidade(int capacidade, int i) {
+        if(i == 1){
+            this.capacidade = capacidade;
+        }
     }
-    public boolean descricaoEmBrancoException(){
-        return descricaoEmBrancoException();
-    };
+    public float getValorFracao() {
+        return valorFracao;
+    }
+    public void setValorFracao(float valorFracao, int i) {
+        if(i == 1) {
+            this.valorFracao = valorFracao;
+        }
+    }
+    public int getPocentagemHoraCheia() {
+        return pocentagemHoraCheia;
+    }
+    public void setPocentagemHoraCheia(int pocentagemHoraCheia, int i) {
+        if(i == 1) {
+            this.pocentagemHoraCheia = pocentagemHoraCheia;
+        }
+    }
+    public float getValorDiaria() {
+        return valorDiaria;
+    }
+    public void setValorDiaria(float valorDiaria, int i) {
+        if(i == 1) {
+            this.valorDiaria = valorDiaria;
+        }
 
-    public double calcularValorContratante( double valorEstacionamento,double valorPorcentagemContratante){
-        this.valorEstacionamento = valorEstacionamento;
-        this.valorPorcentagemContratante = valorPorcentagemContratante;
+    }
+    public int getPorcentagemNoturno() {
+        return porcentagemNoturno;
+    }
+    public void setPorcentagemNoturno(int porcentagemNoturno, int i) {
+        if(i == 1) {
+            this.porcentagemNoturno = porcentagemNoturno;
+        }
+    }
+    public float getValorMesalista() {
+        return valorMesalista;
+    }
 
-        return (valorPorcentagemContratante / 100) * valorEstacionamento;
-    };
+    public void setValorMesalista(float valorMesalista, int i) {
+        if(i == 1){
+            this.valorMesalista = valorMesalista;
+        }
+
+    }
+
+    public float getValorEvento() {
+        return valorEvento;
+    }
+
+    public void setValorEvento(float valorEvento, int i) {
+        if(i == 1){
+            this.valorEvento = valorEvento;
+        }
+    }
+
+    //funções
+    public int validarInt(int numero){
+        int i;
+        if(numero < 0 || numero == 00 ){
+            System.out.println("O campo está em branco ou é inválido");
+            i = 0;
+        }else{
+            i = 1;
+        }
+        return i;
+    }
+    public int validarFloat(float numero){
+        int i;
+        if(numero < 0 || numero == 00 ){
+            System.out.println("O campo está em branco ou é inválido");
+            i = 0;
+        }else{
+            i = 1;
+        }
+        return i;
+    }
+
+    public float calcularHoraCheia(){
+        float x = 4*this.valorFracao - ((this.pocentagemHoraCheia*4*this.valorFracao)/100);
+        return x;
+    }
+
+    public float calcularDiariaNoturna(){
+        float x = ((this.porcentagemNoturno*this.valorDiaria)/100);
+        return x;
+    }
+
+    void status(){
+        System.out.println("Status do estacionamento ");
+        System.out.println("Capacidade máxima: "+ getCapacidade());
+        System.out.println("Valor fração: R$ "+ getValorFracao());
+        System.out.println("Valor hora cheia: R$ "+ calcularHoraCheia());
+        System.out.println("Valor diária diurna: R$ "+ getValorDiaria());
+        System.out.println("Valor diária noturna: R$ "+ calcularDiariaNoturna());
+        System.out.println("Valor do acesso mesalista: R$ "+ getValorMesalista());
+        System.out.println("Valor do acesso evento: R$ "+ getValorEvento());
+    }
 }
--- Projeto-OO/src/Main.java
+++ Projeto-OO/src/Main.java
@@ -1,8 +1,45 @@
 public class Main {
     public static void main(String[] args) {
+        int i;
+        Scanner ler = new Scanner(System.in);
+        Estacionamento e1 = new Estacionamento();
 
-        System.out.println("Commit teste da Dani!");
-    }
+        System.out.println("Digite a quantidade máxima do estacionamento: ");
+        int valorCapacidade = ler.nextInt();
+        i = e1.validarInt(valorCapacidade);
+        e1.setCapacidade(valorCapacidade, i);
 
+        System.out.println("Digite o valor do acesso fração do estacionamento: ");
+        float valorFracao = ler.nextFloat();
+        i = e1.validarFloat(valorFracao);
+        e1.setValorFracao(valorFracao, i);
+
+        System.out.println("Digite o valor da porcentagem da hora cheia do estacionamento: ");
+        int valorHoraCheia= ler.nextInt();
+        i = e1.validarInt(valorHoraCheia);
+        e1.setPocentagemHoraCheia(valorHoraCheia, i);
+
+        System.out.println("Digite o valor da porcentagem da diária noturna do estacionamento: ");
+        int valorNoturno= ler.nextInt();
+        i = e1.validarInt(valorNoturno);
+        e1.setPorcentagemNoturno(valorNoturno, i);
+
+        System.out.println("Digite o valor da diária diurna do estacionamento: ");
+        float valorDiaria = ler.nextFloat();
+        i = e1.validarFloat(valorDiaria);
+        e1.setValorDiaria(valorDiaria, i);
+
+        System.out.println("Digite o valor da acesso evento do estacionamento: ");
+        float valorEvento = ler.nextFloat();
+        i = e1.validarFloat(valorEvento);
+        e1.setValorEvento(valorEvento, i);
+
+        System.out.println("Digite o valor da acesso mensalista do estacionamento: ");
+        float valorMensalista= ler.nextFloat();
+        i = e1.validarFloat(valorMensalista);
+        e1.setValorEvento(valorMensalista, i);
+
+        e1.status();
+        ler.close();
 
 }
